9bb707786fa8d5fb22d2815dc40fd90a94ec65a5
Jim Kang <jimkang@gmail.com>
2018-08-28 20:56:12 -0400

    Fix bug in which analyses never get added to report. (Re)generate reports for each page, given overall doc collection data has changed.

--- archive-tfidf.js
+++ archive-tfidf.js
@@ -3,17 +3,21 @@ var fs = require('fs');
 var Tracker = require('term-tracker');
 
 if (process.argv.length < 3) {
-  console.error('Usage: node archive-tfidf.js <root directory of archive maintained by static-web-archive> [lastPage, a number like 0 or 18]');
+  console.error(
+    'Usage: node archive-tfidf.js <root directory of archive maintained by static-web-archive> [lastPage, a number like 0 or 18]'
+  );
   process.exit();
 }
 
 var archiveDir = process.argv[2];
+var metaDir = archiveDir + '/meta';
+var termDir = archiveDir + '/term-data';
 var lastPage;
 
 if (process.argv.length > 3) {
   lastPage = +process.argv[3];
 } else {
-  lastPage = +fs.readFileSync(archiveDir + '/meta/last-page.txt', { encoding: 'utf8' });
+  lastPage = +fs.readFileSync(metaDir + '/last-page.txt', { encoding: 'utf8' });
 }
 
 if (isNaN(lastPage) || lastPage < 1) {
@@ -23,46 +27,70 @@ if (isNaN(lastPage) || lastPage < 1) {
 
 var pageToAdd = lastPage - 1;
 var lastPageAdded;
-const lastPageAddedPath = archiveDir + '/term-data/most-recent-page-added.txt';
+const lastPageAddedPath = termDir + '/most-recent-page-added.txt';
 
 if (fs.existsSync(lastPageAddedPath)) {
   lastPageAdded = +fs.readFileSync(lastPageAddedPath, { encoding: 'utf8' });
 }
 
 if (!isNaN(lastPageAdded) && lastPageAdded >= pageToAdd) {
-  console.error('Already added this page, not enough has changed to run analysis.');
+  console.error(
+    'Already added this page, not enough has changed to run analysis.'
+  );
   process.exit();
 }
 
-var docs = JSON.parse(fs.readFileSync(`${archiveDir}/meta/${pageToAdd}.json`, { encoding: 'utf8' }));
+var docs = JSON.parse(
+  fs.readFileSync(`${metaDir}/${pageToAdd}.json`, { encoding: 'utf8' })
+);
 
 var tracker = Tracker({
-  storeFile: archiveDir + '/term-data/terms.json',
+  storeFile: termDir + '/terms.json',
   textProp: 'caption'
 });
 
 docs.forEach(tracker.track);
 
-var termsSorted = tracker.getTermsSortedByCount({ limit: 100 });
-console.log(termsSorted);
+//var termsSorted = tracker.getTermsSortedByCount({ limit: 100 });
 
-tracker.save(calculate);
+tracker.save(generateReports);
 
-function calculate(error) {
+function generateReports(error) {
   if (error) {
     console.error('Error saving term tracker:', error);
     return;
   }
 
   fs.writeFileSync(lastPageAddedPath, pageToAdd, { encoding: 'utf8' });
-  
-  docs.map(addTFIDF);
-  var reports = docs.map(getDocReport);
-  console.log(JSON.stringify(reports, null, 2));
+
+  for (var i = 0; i <= pageToAdd; ++i) {
+    calculateReportsForPage(i);
+  }
+}
+
+function calculateReportsForPage(pageNumber) {
+  var pageDocs = JSON.parse(
+    fs.readFileSync(`${archiveDir}/meta/${pageNumber}.json`, {
+      encoding: 'utf8'
+    })
+  );
+  pageDocs.map(addTFIDF);
+  var reports = pageDocs.map(getDocReport);
+  fs.writeFileSync(
+    `${termDir}/page-${pageNumber}-reports.json`,
+    JSON.stringify(reports, null, 2),
+    { encoding: 'utf8' }
+  );
 }
 
 function addTFIDF(doc) {
   var docMeta = tracker.getDocMeta({ id: doc.id });
+  if (!docMeta) {
+    console.error(`Unable to get metadata for document ${doc.id}`);
+    return;
+  } else {
+    //console.error('Found metdata for', doc.id);
+  }
   var analyses = [];
   if (docMeta.termCount > 0 && docs.length > 0) {
     for (var term in docMeta.countsPerTerm) {
@@ -74,7 +102,7 @@ function addTFIDF(doc) {
           term,
           tf,
           idf,
-          tfidf: tf * idf,
+          tfidf: tf * idf
         });
       }
     }
@@ -83,7 +111,10 @@ function addTFIDF(doc) {
 }
 
 function getDocReport(doc) {
-  var sortedAnalyses = doc.analyses.sort(aGoesBeforeB).slice(0, 10);
+  var sortedAnalyses = [];
+  if (doc.analyses) {
+    sortedAnalyses = doc.analyses.sort(aGoesBeforeB).slice(0, 10);
+  }
   return {
     id: doc.id,
     topTerms: sortedAnalyses
@@ -97,4 +128,3 @@ function aGoesBeforeB(a, b) {
     return 1;
   }
 }
-
